1159ea2c682cef8eea816406e034bd4810f6ad6a
Raw Ness <rawness@Raws-Mac-mini.local>
2022-05-10 11:55:43 -0700

    downgrade to node fetch v 2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-// const router = require('express').Router();
 const app = express();
+const fetch = require('node-fetch');
+const ferryURL = 'https://wsdot.wa.gov/ferries/api/schedule/rest/schedule/2022-05-10/4/7?apiaccesscode=e1200b52-935e-4ab9-9289-7314a369fbc4'
 
 app.get('/', (req, res) => {
-    res.json('Hi');
+    fetch(ferryURL)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        res.send(data);
+    });
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const api = require('./routes/')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.use(routes);
 app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}/`))
