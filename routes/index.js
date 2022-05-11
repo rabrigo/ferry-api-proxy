11fe48f02f656caf9413b03309dba51088687322
Raw Ness <rawness@Raws-Mac-mini.local>
2022-05-10 19:01:44 -0700

    created get route for getting schedule

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
-const ferryURL = 'https://wsdot.wa.gov/ferries/api/schedule/rest/schedule/2022-05-10/4/7?apiaccesscode=e1200b52-935e-4ab9-9289-7314a369fbc4'
+const ferryBaseURL = 'https://wsdot.wa.gov/ferries/api/schedule/rest/schedule/';
+const apiKey = 'e1200b52-935e-4ab9-9289-7314a369fbc4';
 
-app.get('/', (req, res) => {
-    fetch(ferryURL)
+app.get('/:date/:departing/:arriving', (req, res) => {
+    fetch(`${ferryBaseURL}/${req.params.date}/${req.params.departing}/${req.params.arriving}?apiaccesscode=${apiKey}`)
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        // console.log(data);
         res.send(data);
     });
 });
