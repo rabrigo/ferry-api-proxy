4267981b19c95cd6f65405ddd8cd510891a9fa6e
Ronnel Abrigo <ronnelabrigo@gmail.com>
2022-05-12 17:20:58 -0700

    renders times in HH:MM am/pm format

--- public/main.js
+++ public/main.js
@@ -3,18 +3,19 @@ const month = date.toLocaleString('en-US', {month: '2-digit'});
 const year = date.getFullYear();
 const day = date.toLocaleString('en-US', {day: '2-digit'});
 date = year + '-' + month + '-' + day;
-// YYYY - MM - DD format
 
-console.log(`The current date is ${date}`);
-
-// regex for getting the characters between the parenthesis
-// from the departuretimes response:  \(([\S]*)\)$
+// console.log(`The current date is ${date}`);
 
 fetch(`/api/${date}/4/7`)
 .then(response => response.json())
 .then(data => {
-    console.log(data);
+    // console.log(data);
     console.log(data.TerminalCombos[0].Times);
-    const firstDeparture = new Date();
-    console.log(data.TerminalCombos[0].Times[0].DepartingTime);
+    for (let i = 0; i < data.TerminalCombos[0].Times.length; i++) {
+        const arrString = data.TerminalCombos[0].Times[i].DepartingTime;
+        // console.log(arrString);
+        const replString = arrString.replaceAll(/[^-0-9]+/g, '');
+        const ferryTime = new Date(parseInt(replString));
+        console.log(`ferry time: ${ferryTime.toLocaleTimeString('en-US', {hour: '2-digit', minute: '2-digit'})}`);
+    }
 });
