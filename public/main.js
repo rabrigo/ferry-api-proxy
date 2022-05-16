bd56bd1c820e36694ad50340fe430a6960991a21
Raw Ness <rawness@Raws-Mac-mini.local>
2022-05-16 13:15:46 -0700

    drop down list

--- public/main.js
+++ public/main.js
@@ -1,38 +1,47 @@
 let rightNow = new Date();
-const month = rightNow.toLocaleString('en-US', {month: '2-digit'});
+const month = rightNow.toLocaleString('en-US', { month: '2-digit' });
 const year = rightNow.getFullYear();
-const day = rightNow.toLocaleString('en-US', {day: '2-digit'});
+const day = rightNow.toLocaleString('en-US', { day: '2-digit' });
 const dateParam = year + '-' + month + '-' + day;
+let departParam;
+let arrivalParam;
+
 // console.log(`The current date is ${dateParam}`);
-console.log(`The time is ${rightNow.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}`);
+// console.log(`The time is ${rightNow.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })}`);
 
 // global variable to check if a scheduled time has passed
 let timePassed = false;
 
-fetch(`/api/${dateParam}/4/7`)
-.then(response => response.json())
-.then(data => {
-    const message = `Departing from ${data.TerminalCombos[0].DepartingTerminalName}, arriving in ${data.TerminalCombos[0].ArrivingTerminalName}`;
-    printMsg(message);
-    console.log(data);
-    // console.log(data.TerminalCombos[0].Times);
-    for (let i = 0; i < data.TerminalCombos[0].Times.length; i++) {
-        const arrString = data.TerminalCombos[0].Times[i].DepartingTime;
-        // console.log(arrString);
-        const replString = arrString.replaceAll(/[^-0-9]+/g, '');
-        const unixString = new Date(parseInt(replString));
-        const ferryTime = unixString.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
- 
-        checkTime(unixString);
-        addFerryTime(ferryTime);
-    }
-});
+// add event listener for drop down list
+// if certain values are selected then departing and arrival terminals
+// are changed in the fetch request
 
-function printMsg(text) {
+function fetchFerries() {
+    fetch(`/api/${dateParam}/4/7`)
+    .then(response => response.json())
+    .then(data => {
+        const message = `Departing from ${data.TerminalCombos[0].DepartingTerminalName}, arriving in ${data.TerminalCombos[0].ArrivingTerminalName} <br> for ${rightNow}`;
+        printDate(message);
+        console.log(data);
+        // console.log(data.TerminalCombos[0].Times);
+        for (let i = 0; i < data.TerminalCombos[0].Times.length; i++) {
+            const arrString = data.TerminalCombos[0].Times[i].DepartingTime;
+            // console.log(arrString);
+            const replString = arrString.replaceAll(/[^-0-9]+/g, '');
+            const unixString = new Date(parseInt(replString));
+            const ferryTime = unixString.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+
+            checkTime(unixString);
+            addFerryTime(ferryTime);
+        }
+    });
+}
+
+function printDate(text) {
     // there's no insertAfter() function that I could find
     const ferryTimes = document.getElementById('ferry-times');
     const mainMsg = document.createElement('h2');
-    mainMsg.innerText = text;
+    mainMsg.innerHTML = text;
     ferryTimes.parentNode.insertBefore(mainMsg, ferryTimes);
 }
 
