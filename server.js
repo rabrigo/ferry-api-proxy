678b1637abaf62b09914e534760ef647a1d96955
rabrigo <rawness@Raws-Mac-mini.local>
2022-07-20 11:33:14 -0700

    fixed public directory

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.send('Connected');
 });
